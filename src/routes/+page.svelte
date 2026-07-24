3f1167945adf1cce1c52181b78c7b0166bc5773a
Pololix <devpololix@gmail.com>
2026-07-24 20:28:58 +0200

    Added visual input box

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let commandLineOpen = $state(true);
+    const callback = (event: KeyboardEvent) => {
+        if (event.key !== ":") return;
+        commandLineOpen = commandLineOpen === true ? false : true;
+    };
+
+    onMount(() => {
+        addEventListener("keydown", callback);
+        return () => {
+            removeEventListener("keydown", callback);
+        };
+    });
+</script>
+
+<main>
+    {#if commandLineOpen}
+        <input class="cmdline" />
+    {/if}
+</main>
+
+<style>
+    .cmdline {
+        position: fixed;
+        left: 5px;
+        right: 5px;
+        bottom: 5px;
+    }
+</style>
