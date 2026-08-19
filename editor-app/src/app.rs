00e90253556351877baf2554b8477d97be0bc59a
Pololix <devpololix@gmail.com>
2026-08-19 23:22:29 +0200

    Moved app management to its own crate

---
+++ editor-app/src/app.rs
@@ -0,0 +1,17 @@
+use editor_core::Editor;
+use editor_renderer::Renderer;
+use editor_window::Window;
+
+pub struct App {
+    // window: Window,
+    // editor: Editor,
+    // renderer: Renderer,
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn run() {}
+}
---
+++ editor-app/src/main.rs
@@ -0,0 +1,6 @@
+mod app;
+
+fn main() {
+    let mut app = app::App::new();
+    app.run();
+}
