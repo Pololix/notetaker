73189f7c7fa78d572a05939e92a3eb7e9f99af71
Pololix <devpololix@gmail.com>
2026-09-01 16:19:23 +0200

    Populated EditorCommand enum

--- editor-core/src/event/command.rs
+++ editor-core/src/event/command.rs
@@ -1,2 +1,33 @@
-#[derive(Debug)]
-pub enum EditorCommand {}
+#[derive(Debug, Clone)]
+pub enum EditorCommand {
+    Quit,
+
+    // workspaces
+    OpenWorkspace,
+    QuitWorkspace,
+
+    // modes
+    EnterNormalMode,
+    EnterPendOpMode,
+    EnterInsertMode,
+    EnterCmdlineMode,
+    EnterTerminalMode,
+
+    // buffers
+    MoveFocus,
+    OpenBuffer,
+    QuitBuffer,
+    SplitBuffer,
+    AddBuffer,
+
+    // buffer ops
+    MoveCursor,
+    Insert,
+    Delete,
+    DeleteSelection,
+    Write,
+
+    Undo,
+    Redo,
+    // todo: plugin commands
+}
